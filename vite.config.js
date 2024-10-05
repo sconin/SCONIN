b5ca047c7ee6dcaa8b3aea58235d23f890210e2f
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2024-10-04 19:17:39 -0500

    before merge with new dir scr/

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
 
 export default defineConfig({
     base: '/SCONIN/',
+    root: resolve(__dirname, "scr"),
+    build: {
+        outDir: "../dist",
+    },
+    server: {
+        port: 6060,
+    }
 });
